207a5191d6b477f132c79bcbb30b1b6d3ecf15b5
Gregorie Calderon <calderon.greg6112@gmail.com>
2021-02-26 21:08:57 -0800

    Implemented twoSum

--- problems.js
+++ problems.js
@@ -1,14 +1,29 @@
 /*
-Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-You can return the answer in any order.
 
  I: Array of numbers and target number
  O: Array of 2 numbers that add up to that number
 
-*/
-var twoSum = function(nums, target) {
+ Create a array for results
+ Iterate over nums array
+ Set a variable for current element
+ Iterate over again
+ if first element and second element is === target
+ push indexs of both elements into result array
+ return
 
+ */
+
+var twoSum = function(nums, target) {
+  var results = [];
+  for (var i = 0; i < nums.length; i++) {
+      let currentNumber = nums[i];
+      for (var j = i; j < nums.length; j++) {
+          let secondNumber = nums[j];
+          if (currentNumber + secondNumber === target) {
+              results.push(i, j);
+              return results;
+          }
+      }
+  }
+  return 'no matches';
 };
